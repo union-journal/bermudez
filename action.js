0451577cc386f43537349bbd8405b8bca77a0b4e
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2020-01-03 17:35:55 -0500

    testing if scrolling works

--- action.js
+++ action.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-    console.log("hello")
+  function disp( divs ) {
+    var imglist = [];
+    for ( var i = 0; i < divs.length; i++ ) {
+      imglist.push( divs[ i ].innerHTML );
+    }
+  }
+   
+  disp( $( "img" ).toArray());
+
+  $(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+    var height = $(window).height();
 
     
+    $(imglist[0]).css({
+        'opacity': ((height - scrollTop) / height)
+    }); 
+
 
   });
+});
