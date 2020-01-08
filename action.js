17b609af3d855ce2a4337787c45db2922d7f1562
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2020-01-08 16:10:09 -0500

    changing the rate

--- action.js
+++ action.js
@@ -12,27 +12,27 @@ $(document).ready(function() {
       $(lister[0]).addClass("visible")
     }
 
-    if (scrollTop > 700 && scrollTop < 900 ){
+    if (scrollTop > 1200 && scrollTop < 1400 ){
       $(lister[1]).removeClass("invisible")
       $(lister[1]).addClass("visible")
     }
 
-    if (scrollTop > 1200 && scrollTop < 1400 ){
+    if (scrollTop > 2200 && scrollTop < 2400 ){
       $(lister[2]).removeClass("invisible")
       $(lister[2]).addClass("visible")
     }
 
-    if (scrollTop > 1700 && scrollTop < 1900 ){
+    if (scrollTop > 3200 && scrollTop < 3400 ){
       $(lister[3]).removeClass("invisible")
       $(lister[3]).addClass("visible")
     }
 
-    if (scrollTop > 2200 && scrollTop < 2400 ){
+    if (scrollTop > 4200 && scrollTop < 4400 ){
       $(lister[4]).removeClass("invisible")
       $(lister[4]).addClass("visible")
     }
 
-    if (scrollTop > 2700 && scrollTop < 3200 ){
+    if (scrollTop > 5200 && scrollTop < 5400 ){
       
     
       $(lister[0]).css({'opacity': (( scrollTop - 2700 ) / 500) }); 
