f3299131c96e40aaa3405745805910f58371cc26
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2020-01-08 15:27:25 -0500

    lister

--- action.js
+++ action.js
@@ -6,12 +6,26 @@ $(document).ready(function() {
     var scrollTop = $(window).scrollTop();
     var height = $(window).height();
 
-    $(lister[0]).css({
-      'opacity': ((height - scrollTop) / height)
-    }); 
+    if (scrollTop > 200 && scrollTop < 700 ){
+      $(lister[0]).css({
+        'opacity': (( scrollTop - 500 ) / 500)
+      }); 
+    }
 
-    if (scrollTop > 500 && scrollTop < 1000 ){
+    if (scrollTop > 700 && scrollTop < 1200 ){
       $(lister[1]).css({
+        'opacity': (( scrollTop - 500 ) / 500)
+      }); 
+    }
+
+    if (scrollTop > 1200 && scrollTop < 1700 ){
+      $(lister[2]).css({
+        'opacity': (( scrollTop - 500 ) / 500)
+      }); 
+    }
+
+    if (scrollTop > 1700 && scrollTop < 2200 ){
+      $(lister[3]).css({
         'opacity': (( scrollTop - 500 ) / 500)
       }); 
     }
